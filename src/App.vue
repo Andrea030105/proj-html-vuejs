2fef3ea3b46ea1b1e146c3b7cf64cb1d6cf61689
Andrea030105 <tummoloandrea05@gmail.com>
2026-02-04 12:37:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  }
+}
+</script>
+<template>
+  <AppHeader />
+</template>
+<style lang="scss"></style>
