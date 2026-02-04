90825b302faf43132a820cad4332865de265a4cd
Andrea030105 <tummoloandrea05@gmail.com>
2026-02-04 16:15:03 +0100

    header-done-whit-dropdown

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,25 +7,49 @@ export default {
             store,
         }
     },
+    methods: {
+        toggleMenu(index) {
+            // Chiudi tutti gli altri menu
+            store.menuHeaderArray.forEach((item, i) => {
+                if (i !== index) {
+                    item.active = false;
+                }
+            });
+            // Toggle del menu corrente
+            store.menuHeaderArray[index].active = !store.menuHeaderArray[index].active;
+        },
+        closeAllMenus() {
+            store.menuHeaderArray.forEach(item => {
+                item.active = false;
+            });
+        }
+    }
 }
 </script>
 <template>
     <header class="p-4">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-2">
+                <div class="col-2 d-flex align-items-center justify-content-center">
                     <div class="container-logo">
                         <img src="../assets/logo.svg" alt="logo">
                     </div>
                 </div>
                 <div class="col-8 d-flex align-items-center justify-content-center">
                     <ul>
-                        <li v-for="(item, index) in store.menuHeaderArray" :key="index"
-                            class="text-uppercase fw-bolder">
-                            {{ item.name }}</li>
+                        <li @click="toggleMenu(index)" v-for="(item, index) in store.menuHeaderArray" :key="index"
+                            class="text-uppercase fw-bolder me-4">
+                            {{ item.name }}
+                            <i v-if="item.dropDown" class="fa-solid fa-angle-down"></i>
+                            <ul class="dropdown" v-if="(item.active && item.subLink)">
+                                <li v-for="(itemSub, indexSub) in item.subLink" :key="indexSub">
+                                    {{ itemSub }}
+                                </li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
-                <div class="col-2">
+                <div class="col-2 d-flex align-items-center justify-content-center">
                     <div class="icons-list">
                         <ul>
                             <li><i class="fa-solid fa-magnifying-glass"></i></li>
@@ -42,6 +66,9 @@ export default {
     </header>
 </template>
 <style lang="scss" scoped>
+@use '../style/patrial/variables' as *;
+@use '../style/patrial/mixin' as *;
+
 header {
     height: 75px;
 
@@ -61,6 +88,39 @@ header {
         li {
             margin-right: 1rem;
             font-size: .8rem;
+            position: relative;
+
+            &:hover {
+                cursor: pointer;
+            }
+        }
+
+        .dropdown {
+            width: 200px;
+            max-height: 100px;
+            position: absolute;
+            top: 48px;
+            left: 0;
+            background-color: $blackCl;
+            color: $whiteCl;
+            flex-direction: column;
+            align-items: start;
+
+            li {
+                width: 100%;
+                padding: 0 1rem;
+                margin: .5rem 0;
+
+                &:hover {
+                    color: $grayCl;
+                }
+            }
+        }
+    }
+
+    .icons-list {
+        li {
+            font-size: 1.2rem;
         }
     }
 }
--- src/store.js
+++ src/store.js
@@ -4,24 +4,35 @@ export const store = reactive({
   menuHeaderArray: [
     {
       name: "Home",
+      active: false,
     },
     {
       name: "Blog",
+      active: false,
     },
     {
       name: "Pages",
+      active: false,
+      dropDown: true,
+      subLink: ["service", "Our Work Folw", "Video Thubmnail type"],
     },
     {
       name: "About",
+      active: false,
     },
     {
       name: "Contacts",
+      active: false,
     },
     {
       name: "Gallery",
+      active: false,
     },
     {
       name: "Shop",
+      active: false,
+      dropDown: true,
+      subLink: ["Shop Page", "Product Type"],
     },
   ],
 });
